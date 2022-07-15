6ca087b87142ba09067e0a9b7545a62f565dbb6e
Hansol Suh <hansolsuh@lawn-128-61-59-14.lawn.gatech.edu>
2022-07-15 09:44:49 -0400

    fix

--- tos.py
+++ tos.py
@@ -21,18 +21,76 @@ def atos(
     max_iter_backtracking=100,
     h_Lipschitz=None,
     barrier=None,
+    inner_aa=None,
+    outer_aa=None
 ):
     success = False
+    if inner_aa is None:
+        inner_aa = 0
+    if outer_aa is None:
+        outer_aa = 0
+    
     z_old = x0
     z = prox_2(x0, step_size )
     fk,grad_fk = f_grad(z)
-    x = prox_1(z-step_size* grad_fk, step_size )
+    y = z-step_size*grad_fk
+    x = prox_1(y, step_size )
     u = np.zeros_like(x)
 
+    if inner_aa !=0:
+        p = y
+        p_aa   = []
+        Q_aa   = []
+
+    if outer_aa !=0:
+        #Safeguard from Fu,Zhang,Boyd, 2020
+        safeguard = True
+        n_aa      = 0
+        iter_aa   = 0 #R_aa in Fu paper
+        D_aa      = 1.e7 #TODO choose?
+        R_safe    = 10
+        r         = (x-z)/step_size #TODO making up u0
+        r0_norm = norm(r)
+        eps_aa = 1.e-6
+        R_aa = []
+        u_aa = []
+
     for it in range(max_iter):
+        aa_mk_inner = min(inner_aa,it)
+        aa_mk_outer = min(outer_aa,it)
+
         grad_fk_old = grad_fk
         fk,grad_fk = f_grad(z)
-        x = prox_1(z- step_size*(u+grad_fk), step_size )
+
+        if inner_aa != 0:
+            p_old = p
+            p = z-step_size*(u+grad_fk)
+            q = p - y
+            len_Q = len(Q_aa)
+            if len_Q >= aa_mk_inner and len_Q != 0:
+                Q_aa.pop(0)
+            Q_aa.append(q)
+            Q_sol = AA_LQ(Q_aa,1.e-6)
+
+            if len(p_aa) >= aa_mk_inner and len(p_aa) != 0:
+                p_aa.pop(0)
+            p_aa.append(p)
+            y_test = sum([p_aa[i]*val for i,val in enumerate(Q_sol)])
+
+        if inner_aa == 0:
+            x = prox_1(z- step_size*(u+grad_fk), step_size)
+        else:
+            x_test = prox_1(y_test, step_size)
+            fx_test = f_grad(x_test, return_gradient=False) 
+            fx,grad_fk = f_grad(x)
+            incr = x-z
+            f_if_test = fx_test - fx - grad_fk.dot(incr) - (norm(incr)**2)/(2*step_size)
+            if f_if_test <= 0:
+                x = x_test
+            else:
+                x = prox_1(p, step_size)
+                y = p
+
         incr = x-z
         norm_incr = np.linalg.norm(incr)
         
@@ -47,8 +105,39 @@ def atos(
                 else:
                     step_size *= backtracking_factor
 
-        z_old = z
         z = prox_2(x+step_size*u, step_size )
+        u_old = u
+        u += (x-z)/step_size
+
+        #Doing outer on u only
+        if outer_aa != 0:
+            r = u - u_old
+            len_R = len(R_aa)
+            if len_R >= aa_mk_outer and len_R !=0:
+                R_aa.pop(0)
+            R_aa.append(r)
+            R_sol = AA_LQ(R_aa,1.e-6)
+
+            if len(u_aa) >= aa_mk_outer and len(u_aa) != 0:
+                u_aa.pop(0)
+            u_aa.append(u)
+            u_test = sum([u_aa[i]*val for i,val in enumerate(R_sol)])
+
+            #Safeguarding
+            if safeguard or iter_aa >= R_safe:
+                if norm(r) <= D_aa*r0_norm*(n_aa/R_safe + 1)**(-1-eps_aa):
+                    u = u_test
+                    n_aa = n_aa + 1
+                    safeguard = False
+                    R_safe = 1
+                else:
+                    #z = z_TOS
+                    R_safe = 0
+            else:
+                u = u_test
+                n_aa = n_aa + 1
+                R_safe = R_safe + 1
+
         certificate = norm_incr / step_size
         if ls and h_Lipschitz is not None:
             if h_Lipschitz == 0:
